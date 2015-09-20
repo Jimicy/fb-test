dff071f2baa1b234a53ddf55df78f16a07993231
Jimicy <jimmy_wang1995@hotmail.com>
2015-09-19 22:34:55 -0400

    Fix fb api user events.

--- script.js
+++ script.js
@@ -80,7 +80,7 @@ var myDataRef = new Firebase('https://shining-inferno-4500.firebaseio.com/');
           var picture = response.picture.data.url;
           var events = [];
           if (response.events) {
-            response.events.data;
+            events = response.events.data;
           }
           myDataRef.child(response.id).set({picture: picture, events: events});
         }
